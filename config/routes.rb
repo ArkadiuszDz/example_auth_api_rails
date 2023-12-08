5d1714d14750a556c8625eb8582cb366918da10d
Arkadiusz Dziechciarz <arkadiusz.dziechciarz@gmail.com>
2023-12-08 18:29:54 +0100

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get "/test", to: "example#example_handler"
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+end
